7e898c585640d30d9ceb7a177dd07d2bfc22f5c2
Michel Wilhelm <michelwilhelm@gmail.com>
2013-03-19 10:36:18 -0700

    Criando formulário para cadastro de usuários

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -24,4 +24,57 @@ class ControllerUsers extends Controllers
 		    
 		    Layout::mainFooter();
 		}
+		
+		public static function newUser()
+		{
+		    $_error = false;
+		    if (!empty($_POST) && count($_POST) === 4) {
+		        if (empty($_POST['name']) || empty($_POST['email']) ) {
+		            $_error = true;
+		        }
+		        
+		        if (
+		            empty($_POST['password']) || 
+		            $_POST['password'] == '' ||
+                empty($_POST['re_password']) ||
+                $_POST['re_password'] == ''
+		        ) {
+		            
+		        }
+		        
+		        if ($_POST['password'] != $_POST['re_password']) {
+		            $_error = true;
+		        }
+		        
+		        if ($_error == true) {
+		            self::redirect(self::location('users/signin', true));
+		        } else {
+		            
+                // Preparing variables for insert
+		            $_POST['password'] = self::passwd($_POST['password']);
+		            $_POST['email'] = self::str_email($_POST['email']);
+		            $_POST['name']  = trim($_POST['name']);
+		            
+		            $_data = $_POST;
+		            $_data['created'] = new MongoDate(time());
+		            $_data['updated'] = new MongoDate(time());
+		            $_data['active']  = true;
+		            $_data['roles'] = Array(
+                    'is_admin' => false,
+		                'is_customer' => true,
+		                'is_manager'  => false,
+		                'is_developer' => false,
+		                'create_task' => true,
+                    'create_project' => false,
+		                'create_ticket'  => true
+		            );
+		            $_data['locale'] = 'UTC';
+		            
+		            // Verify if user exists
+		            self::pr($_data);
+		            
+		        }
+		    }
+		    
+		}
 }
